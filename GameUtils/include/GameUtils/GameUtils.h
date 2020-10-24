3698a9dfdfaec1a9c1e7c12ea86705e75322ef88
benjamin <benjamin.zamour@gmail.com>
2020-10-25 01:54:48 +0200

    Added missing header guards to GameUtils.h

--- GameUtils/include/GameUtils/GameUtils.h
+++ GameUtils/include/GameUtils/GameUtils.h
@@ -1,6 +1,11 @@
+#ifndef GAMEUTILS_GAMEUTILS_H
+#define GAMEUTILS_GAMEUTILS_H
+
 #include <iostream>
 
 namespace GameUtils
 {
     std::ostream &greet(std::ostream &out);
 }
+
+#endif // GAMEUTILS_GAMEUTILS_H
